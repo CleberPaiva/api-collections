8cab96517ca1e629f2b9046bb52c563e2996ace2
Cleber Paiva <paivadeap@gmail.com>
2021-03-14 20:06:50 -0300

    Teste Operacoes 2

--- src/main/kotlin/dio/cleberpaiva/collections/TesteOperacoes.kt
+++ src/main/kotlin/dio/cleberpaiva/collections/TesteOperacoes.kt
@@ -16,4 +16,16 @@ fun main() {
     println("------------------")
     val salariosMaiorque2500 = salarios.filter { it > 2500.0 }
     salariosMaiorque2500.forEach { println(it)}
+
+
+    println("------------------")
+    println(salarios.count { it in 2000.0..5000.0})
+
+    println("------------------")
+    println(salarios.find { it == 3240.0})
+    println(salarios.find { it == 3440.0})
+
+    println("------------------")
+    println(salarios.any { it == 1000.0})
+    println(salarios.any { it == 500.0})
 }
