dd6e9f2db658c4f6143af8f9ec2395777382041e
CleberPaiva <paivadeap@gmail.com>
2021-03-17 18:39:38 -0300

    Teste Funcoes Extendidas

---
+++ src/main/kotlin/dio/cleberpaiva/collections/TesteFuncoesExtendidas.kt
@@ -0,0 +1,16 @@
+package dio.cleberpaiva.collections
+
+fun main() {
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "1500".toBigDecimal(),
+        "4000".toBigDecimal(),
+    )
+
+    println("----------- Somatoria -----------")
+    println(salarios.somatoria())
+
+    println("----------- Media -----------")
+    println(salarios.media())
+
+}
